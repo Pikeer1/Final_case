d0930af707a283849fcaabca102a401b7b02267d
Paul <f1rstmarious@yandex.ru>
2022-10-10 14:45:26 +0300

    code task completed

---
+++ zadanie/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] firstArray = new string[5] {"Russia", "345", "15", "lol", "computer"};
+string[] secondArray = new string[firstArray.Length];
+void GetSecondArr(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+    if(firstArray[i].Length <= 3)
+        {
+        secondArray[count] = firstArray[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+GetSecondArr(firstArray, secondArray);
+PrintArray(secondArray);
